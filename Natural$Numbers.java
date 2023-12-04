330b6e9554db3332e6743ed0e32ab587fe3d9aac
TirumalaManchigorla <139000551+TirumalaManchigorla@users.noreply.github.com>
2023-12-04 11:14:47 +0530

    Add files via upload

---
+++ Natural$Numbers.java
@@ -0,0 +1,13 @@
+class Natural$Numbers
+{
+	public static void main(String[] args) 
+	{
+		for (int a=1;a<=10 ;a++ )
+		{
+			if (a!=7)
+			{
+				System.out.println("The Natural Numbers of 1 to 10 is : "+(a));
+			}
+		}
+	}
+}
